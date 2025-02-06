635191ec78ab2f1247128cd3be733db1ac2e20cf
cafebo <cafebo@alumnatflorida.es>
2025-02-06 11:03:41 +0100

    hola5

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -1,8 +1,18 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use App\DNI;
+
 class DNITest extends TestCase {
-    public function TestDNIConLetra() {
-        return $this->dniConLetra;
+    public function testDNIConLetra() {
+        $dni = new DNI(12345678); 
+        $this->assertEquals("12345678Z", $dni->getDNIConLetra());
+
+        $dni2 = new DNI(87654321);
+        $this->assertEquals("87654321X", $dni2->getDNIConLetra());
+    }
+
+    public function testDNIConLetraInvalid() {
+        $this->expectException(TypeError::class);
+        new DNI("ABCDE123");
     }
 }
